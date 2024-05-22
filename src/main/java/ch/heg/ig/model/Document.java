192c481c4c75f0baf43252396d86976606d14562
Darwin Ameli <darwin.ameli@rpn.ch>
2024-05-22 15:39:06 +0200

    add getters setters

--- src/main/java/ch/heg/ig/model/Document.java
+++ src/main/java/ch/heg/ig/model/Document.java
@@ -3,11 +3,76 @@ package ch.heg.ig.model;
 import java.util.List;
 
 public class Document {
-    private int objectId;
-    private int originalId;
+    private int objectID;
+    private int originalID;
+    private int contentTypeID;
     private String contentType;
-    private boolean isLastVersion;
+    private String creationDate;
     private String author;
     private List<Field> fields;
+    private String attachmentFileName;
 
+    public int getObjectID() {
+        return objectID;
+    }
+
+    public void setObjectID(int objectID) {
+        this.objectID = objectID;
+    }
+
+    public int getOriginalID() {
+        return originalID;
+    }
+
+    public void setOriginalID(int originalID) {
+        this.originalID = originalID;
+    }
+
+    public int getContentTypeID() {
+        return contentTypeID;
+    }
+
+    public void setContentTypeID(int contentTypeID) {
+        this.contentTypeID = contentTypeID;
+    }
+
+    public String getContentType() {
+        return contentType;
+    }
+
+    public void setContentType(String contentType) {
+        this.contentType = contentType;
+    }
+
+    public String getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(String creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public List<Field> getFields() {
+        return fields;
+    }
+
+    public void setFields(List<Field> fields) {
+        this.fields = fields;
+    }
+
+    public String getAttachmentFileName() {
+        return attachmentFileName;
+    }
+
+    public void setAttachmentFileName(String attachmentFileName) {
+        this.attachmentFileName = attachmentFileName;
+    }
 }
